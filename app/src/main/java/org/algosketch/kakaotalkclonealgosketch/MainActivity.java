b7442099cd74039727b593709531e4f4c35ebb0c
algosketch <sjy_9484@naver.com>
2020-10-14 00:33:57 +0900

    UI - TabHost

--- app/src/main/java/org/algosketch/kakaotalkclonealgosketch/MainActivity.java
+++ app/src/main/java/org/algosketch/kakaotalkclonealgosketch/MainActivity.java
@@ -1,8 +1,10 @@
 package org.algosketch.kakaotalkclonealgosketch;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.res.ResourcesCompat;
 
 import android.os.Bundle;
+import android.widget.TabHost;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TabHost host = findViewById(R.id.host);
+        host.setup();
+
+        TabHost.TabSpec spec = host.newTabSpec("tab1");
+        spec.setIndicator(null, ResourcesCompat.getDrawable(getResources(), R.drawable.ic_person, null));
+        spec.setContent(R.id.tab_content1);
+        host.addTab(spec);
+
+        spec = host.newTabSpec("tab2");
+        spec.setIndicator(null, ResourcesCompat.getDrawable(getResources(), R.drawable.ic_speech_bubble, null));
+        spec.setContent(R.id.tab_content2);
+        host.addTab(spec);
+
+        spec = host.newTabSpec("tab3");
+        spec.setIndicator(null, ResourcesCompat.getDrawable(getResources(), R.drawable.ic_pound, null));
+        spec.setContent(R.id.tab_content3);
+        host.addTab(spec);
+
+        spec = host.newTabSpec("tab4");
+        spec.setIndicator(null, ResourcesCompat.getDrawable(getResources(), R.drawable.ic_etc, null));
+        spec.setContent(R.id.tab_content4);
+        host.addTab(spec);
     }
 }
