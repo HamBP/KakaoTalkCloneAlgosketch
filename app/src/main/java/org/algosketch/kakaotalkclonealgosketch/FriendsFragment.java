dcba36745b9adb621ca4d933747e6617eafe54c8
algosketch <sjy_9484@naver.com>
2020-10-23 11:47:39 +0900

    FriendsFragment.java - 커스텀 아이템 적용

--- app/src/main/java/org/algosketch/kakaotalkclonealgosketch/FriendsFragment.java
+++ app/src/main/java/org/algosketch/kakaotalkclonealgosketch/FriendsFragment.java
@@ -1,16 +1,16 @@
 package org.algosketch.kakaotalkclonealgosketch;
 
-import android.graphics.Rect;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.LinearLayout;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.core.view.ViewCompat;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -25,9 +25,18 @@ public class FriendsFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_friends, null);
 
         RecyclerView recyclerView = view.findViewById(R.id.recyclerview_friends);
-        List<String> list = new ArrayList<>();
+        List<ItemVO> list = new ArrayList<>();
         for(int i = 0; i < 20; ++i) {
-            list.add("friend " + i);
+            ItemVO vo = new ItemVO();
+
+            BitmapFactory.Options options = new BitmapFactory.Options();
+            options.inJustDecodeBounds = false;
+            Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.ic_selected_speech_bubble, options);
+            vo.setProfileImage(bitmap);
+
+            vo.setName("김이름");
+            vo.setProfileMassage("상태 메시지");
+            list.add(vo);
         }
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
         recyclerView.setAdapter(new RecyclerViewAdapter(list));
@@ -35,24 +44,26 @@ public class FriendsFragment extends Fragment {
         return view;
     }
 
-    private class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewHolder> {
-        private List<String> list;
+    private class RecyclerViewAdapter extends RecyclerView.Adapter<FriendsViewHolder> {
+        private List<ItemVO> list;
 
-        public RecyclerViewAdapter(List<String> list) {
+        public RecyclerViewAdapter(List<ItemVO> list) {
             this.list = list;
         }
 
         @NonNull
         @Override
-        public RecyclerViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-            View view = LayoutInflater.from(parent.getContext()).inflate(android.R.layout.simple_list_item_1, parent, false);
-            return new RecyclerViewHolder(view);
+        public FriendsViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_friend, parent, false);
+            return new FriendsViewHolder(view);
         }
 
         @Override
-        public void onBindViewHolder(@NonNull RecyclerViewHolder holder, int position) {
-            String text = list.get(position);
-            holder.title.setText(text);
+        public void onBindViewHolder(@NonNull FriendsViewHolder holder, int position) {
+            ItemVO vo = list.get(position);
+            holder.profileImage.setImageBitmap(vo.profileImage);
+            holder.name.setText(vo.name);
+            holder.profileMassage.setText(vo.profileMassage);
         }
 
         @Override
@@ -61,13 +72,35 @@ public class FriendsFragment extends Fragment {
         }
     }
 
-    // class
-    private class RecyclerViewHolder extends RecyclerView.ViewHolder {
-        public TextView title;
+    class ItemVO {
+        Bitmap profileImage;
+        String name;
+        String profileMassage;
 
-        public RecyclerViewHolder(@NonNull View itemView) {
+        public void setProfileImage(Bitmap profileImage) {
+            this.profileImage = profileImage;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public void setProfileMassage(String profileMassage) {
+            this.profileMassage = profileMassage;
+        }
+    }
+
+    // class
+    private class FriendsViewHolder extends RecyclerView.ViewHolder {
+        public ImageView profileImage;
+        public TextView name;
+        public TextView profileMassage;
+
+        public FriendsViewHolder(@NonNull View itemView) {
             super(itemView);
-            title = itemView.findViewById(android.R.id.text1);
+            profileImage = itemView.findViewById(R.id.img_profile);
+            name = itemView.findViewById(R.id.txt_name);
+            profileMassage = itemView.findViewById(R.id.txt_profile_massage);
         }
     }
 
